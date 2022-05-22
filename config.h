d3aa26e3e06e6815cedb2b61807baaaf877f1f78
Moarbue <tomkaufmann1206@gmail.com>
2022-05-22 20:35:51 +0200

    Added config file for program configuration

---
+++ config.h
@@ -0,0 +1,22 @@
+#ifndef CUBE_SIM_CONFIG_H
+#define CUBE_SIM_CONFIG_H
+
+#define CUBE_LENGTH 3
+
+#define ANIMATION_LENGTH .2f
+
+#define SCR_WIDTH   900
+#define SCR_HEIGHT  700
+
+#define MAX_FPS     90
+
+#define BACKGROUND_COLOR ((vec3){1.f, 1.f, 1.f})
+
+#define UP    ((vec3){1.0f , 1.0f , 0.0f})
+#define LEFT  ((vec3){0.0f , 0.0f , 1.0f})
+#define FRONT ((vec3){0.73f, 0.0f , 0.0f})
+#define DOWN  ((vec3){1.0f , 1.0f , 1.0f})
+#define RIGHT ((vec3){0.0f , 0.4f , 0.0f})
+#define BACK  ((vec3){1.0f , 0.35f, 0.0f})
+
+#endif // CUBE_SIM_CONFIG_H
